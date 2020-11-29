0448ee4a9a679de70171e9ed77ae5a840b869118
Bror Brurberg <bror@hey.com>
2020-11-29 12:51:28 +0100

    trying twitch stream

--- src/views/Cameras.svelte
+++ src/views/Cameras.svelte
@@ -3,5 +3,13 @@ import DashboardTile from "../components/DashboardTile.svelte";
 
 </script>
 <div class="flex justify-center h-100 w-100">
-  <iframe src="https://camstreamer.com/embed/169534419011c5d/S-47654?autoplay=1&mute=1" style="border:none; overflow:hidden; width:560px; height:315px" allowfullscreen></iframe>
+  <iframe
+    title="Live Vangslia"
+    src="https://player.twitch.tv/?channel=oppdal_skisenter&parent=www.quirky-cray-70492d.netlify.app"
+    frameborder="0"
+    allowfullscreen="true"
+    scrolling="no"
+    height="378"
+    width="620"
+    ></iframe>
 </div>
