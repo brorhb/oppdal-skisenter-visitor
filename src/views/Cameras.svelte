3b89af87d9f6f90430b7106b1fefca8d927da3ad
Bror Brurberg <bror@hey.com>
2020-11-29 13:42:25 +0100

    testing twitch again

--- src/views/Cameras.svelte
+++ src/views/Cameras.svelte
@@ -2,11 +2,7 @@
 import DashboardTile from "../components/DashboardTile.svelte";
 
 </script>
+
 <div class="flex justify-center h-100 w-100">
-  <iframe
-    width="560"
-    height="315"
-    src="https://www.youtube.com/embed/U4_hESj9D90"
-    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-    allowfullscreen></iframe>
+  <iframe src="https://player.twitch.tv/?channel=oppdal_skisenter&parent=quirky-cray-70492d.netlify.app" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
 </div>
