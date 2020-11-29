27b8940743c505e1ff12d81fd1199d95c776b482
Bror Brurberg <bror@hey.com>
2020-11-29 12:54:00 +0100

    No cookie

--- src/views/Cameras.svelte
+++ src/views/Cameras.svelte
@@ -4,12 +4,9 @@ import DashboardTile from "../components/DashboardTile.svelte";
 </script>
 <div class="flex justify-center h-100 w-100">
   <iframe
-    title="Live Vangslia"
-    src="https://player.twitch.tv/?channel=oppdal_skisenter&parent=www.quirky-cray-70492d.netlify.app"
-    frameborder="0"
-    allowfullscreen="true"
-    scrolling="no"
-    height="378"
-    width="620"
-    ></iframe>
+    width="560"
+    height="315"
+    src="https://www.youtube.com/embed/U4_hESj9D90"
+    frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+    allowfullscreen></iframe>
 </div>
