cc898d1430ae3480b70cb6336ad07014efb03604
Bror Brurberg <bror@hey.com>
2020-11-30 07:21:12 +0100

    autoplay added

--- src/views/Cameras.svelte
+++ src/views/Cameras.svelte
@@ -4,5 +4,5 @@ import DashboardTile from "../components/DashboardTile.svelte";
 </script>
 
 <div class="flex justify-center h-100 w-100">
-  <iframe src="https://player.twitch.tv/?channel=oppdal_skisenter&parent=quirky-cray-70492d.netlify.app" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
+  <iframe src="https://player.twitch.tv/?channel=oppdal_skisenter&parent=quirky-cray-70492d.netlify.app&autoplay=true" frameborder="0" allowfullscreen="true" scrolling="no" height="378" width="620"></iframe>
 </div>
