f9236bce85ada9af70eb8e63394809dd299214d3
Bror Brurberg <bror@hey.com>
2020-12-01 13:34:39 +0100

    url fix

--- src/views/Cameras.svelte
+++ src/views/Cameras.svelte
@@ -1,12 +1,9 @@
 <script>
-import DashboardTile from "../components/DashboardTile.svelte";
-
 </script>
-
 <div class="flex justify-center h-100 w-100">
   <iframe
     title="Livestream Vangslia"
-    src="https://player.twitch.tv/?channel=oppdal_skisenter&parent=quirky-cray-70492d.netlify.app&autoplay=true"
+    src={`https://player.twitch.tv/?channel=oppdal_skisenter&parent=${window.location.host}&autoplay=true`}
     frameborder="0"
     allowfullscreen="true"
     scrolling="no"
