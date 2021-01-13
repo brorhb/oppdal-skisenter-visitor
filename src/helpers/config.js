fb4b27f90967ca1890f912bad7f0306f4b8dcbaf
Bror Brurberg <bror@hey.com>
2021-01-13 17:06:28 +0100

    added support for multiple cameras

--- src/helpers/config.js
+++ src/helpers/config.js
@@ -1,6 +1,6 @@
 const config = {
   BASE_URL: "https://oppdal-skisenter.herokuapp.com/v1",
   //BASE_URL: "http://localhost:5001/v1",
-  CAMERA_URL: `https://player.twitch.tv/?channel=oppdal_skisenter&parent=${window.location.host}&autoplay=true`
+  //CAMERA_URL: `https://player.twitch.tv/?channel=oppdal_skisenter&parent=${PARENT}&autoplay=true`
 }
 export default config
