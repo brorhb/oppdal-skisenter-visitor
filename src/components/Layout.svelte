bac5a48cc2fb7c3dc2695ea20aa36537d5208191
Morten Nordseth <morten@nordseth.net>
2021-07-01 14:11:54 +0200

    refactorization

--- src/components/Layout.svelte
+++ src/components/Layout.svelte
@@ -2,11 +2,29 @@
   import { Route } from 'svelte-router-spa'
   import Footer from './Footer.svelte'
   import Navbar from './Navbar.svelte'
+  import Alert from './Alert.svelte'
+  import { makeAlertStore } from '../stores/AlertStore'
+  import { onDestroy, onMount } from 'svelte'
   export let currentRoute
   export const params = {}
+  let alertStore = makeAlertStore();
+  let alerts = []
+  let unsubscribe
+  onDestroy(() => {
+    if(unsubscribe) {
+      unsubscribe()
+      unsubscribe = null
+    }
+	});
+  onMount(() => {
+    unsubscribe = alertStore.subscribe((data) => alerts = data.filter(alert => alert.is_live))
+  })
 </script>
 <div class="layout">
   <Navbar />
+  {#each alerts as alert}
+    <Alert alert={alert}/>
+  {/each}
   <section>
     <Route {currentRoute}  {params} />
   </section>
--- src/views/Dashboard/index.svelte
+++ src/views/Dashboard/index.svelte
@@ -2,52 +2,17 @@
   import { makeTracksStore } from '../../stores/TrackStore'
   import { makeLiftsStore } from '../../stores/LiftsStore'
   import { makeZoneStore } from '../../stores/ZoneStore'
-  import { makeAlertStore } from '../../stores/AlertStore'
-  import { navigateTo } from 'svelte-router-spa'
   import { onDestroy, onMount } from 'svelte'
   import Large from "./large.svelte"
   import Small from "./small.svelte"
-  import Alert from '../../components/Alert.svelte'
-  import Avalanche from '../../components/Avalanche.svelte'
-  import Zone from '../../components/Zone.svelte'  
+
   let unsubscribe
   let tracksStore = makeTracksStore()
   let liftsStore = makeLiftsStore()
   let zoneStore = makeZoneStore()
-  let alertStore = makeAlertStore();
   let tracks = []
   let zones = []
-  let activeZone = false
   let lifts = []
-  let alerts = []
-  $: trackGroups = tracksInZone.reduce((acc, curr) => {
-    const difficulty = curr.difficulty
-    if (!acc) acc = {}
-    if (acc[difficulty]) acc[difficulty] = [...acc[difficulty], curr]
-    else acc[difficulty] = [curr]
-    return acc
-  }, {})
-  $: liftGroups = liftsInZone.reduce((acc, curr) => {
-    const type = curr.type
-    if (!acc) acc = {}
-    if (acc[type]) acc[type] = [...acc[type], curr]
-    else acc[type] = [curr]
-    return acc
-  }, {})
-  $: tracksInZone = tracks.reduce((acc, track) => {
-    if (!activeZone) acc.push(track)
-    else {
-      if (track.zone === activeZone) acc.push(track)
-    }
-    return acc
-  }, [])
-  $: liftsInZone = lifts.reduce((acc, lift) => {
-    if (!activeZone) acc.push(lift)
-    else {
-      if (lift.zone === activeZone) acc.push(lift)
-    }
-    return acc
-  }, [])
   let innerWidth
 
 
@@ -68,82 +33,23 @@
     zoneStore.subscribe((data) => {
       zones = data
     })
-    alertStore.subscribe((data) => {
-      alerts = data
-    })
   })
-  
-  function selectedTrack(obj) {
-    if (obj.detail.type) {
-      navigateTo(`/lifts/${obj.detail.id}`)
-    } else {
-      navigateTo(`/tracks/${obj.detail.id}`)
-    }
-  }
+
 </script>
 
 <svelte:window bind:innerWidth={innerWidth} />
 <div class="wrapper">
-  {#each alerts as alert}
-    {#if alert.is_live}
-      <Alert alert={alert}/>
-    {/if}
-  {/each}
-  <div>
-    <div class="zones">
-      <div
-        class={`nav-item ${!activeZone ? "active-item" : ""}`}
-        on:click={() => activeZone = false}
-      >
-        Alle
-      </div>
-      {#each zones as zone}
-        <div
-          class={`nav-item ${activeZone === zone.id ? "active-item" : ""} `}
-          on:click={() => activeZone = zone.id}
-        >
-          {zone.name}
-        </div>
-      {/each}
-    </div>
-    {#if innerWidth >= 990}
-        <Large {trackGroups} tracks={tracksInZone} {liftGroups} lifts={liftsInZone} {selectedTrack} />
-      {:else}
-        <Small {trackGroups} tracks={tracksInZone} {liftGroups} lifts={liftsInZone} {selectedTrack} activeZone={activeZone} />
-      {/if}
-  </div>
-  <Avalanche/>
-  <Zone zone={zones[3]}/>
-  <Zone zone={zones[0]}/>
-  <Zone zone={zones[2]}/>
-  
+  {#if innerWidth >= 990}
+    <Large tracks={tracks} lifts={lifts} zones={zones}/>
+  {:else}
+    <Small tracks={tracks} lifts={lifts} activeZone={zones} />
+  {/if}
 </div>
 
 
 <style>
   .wrapper {
-    margin-left: 20px;
-    margin-right: 20px;
-  }
-  .box-shadow {
-    box-shadow: 0px 0px 7px 0px rgba(0,0,0,0.75);
-  }
-  
-  .zones {
-    display: flex;
-    justify-content: center;
-    
-  }
-  .nav-item {
-    color: #004A7C;
-    cursor: pointer;
-    margin: 20px;
-    font-size: 26px;
-  }
-  .nav-item:hover {
-    border-bottom: 1px solid #004A7C;
-  }
-  .active-item {
-    border-bottom: 1px solid #004A7C;
+    margin-left: 50px;
+    margin-right: 50px;
   }
 </style>
