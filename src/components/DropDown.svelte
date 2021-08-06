9f60124deead421272db36d8144acb986ed85d42
mathildehaugum <mathilde_haugum@hotmail.com>
2021-08-06 08:13:35 +0200

    Implement zone component

--- src/components/DropDown.svelte
+++ src/components/DropDown.svelte
@@ -2,18 +2,11 @@
   import { onMount } from 'svelte';
   export let selected_option;
   export let options;
-  export let chosen_zone;
-  let init_option;
+  export let zones;
+  export let lifts; 
+  let init_option = selected_option;
   let root;
 
-  $: selected_option = updateSelected(chosen_zone);
-
-  function updateSelected(chosen_zone) {
-    if (chosen_zone) {
-      selected_option = chosen_zone;
-      init_option = selected_option;
-    };
-  };
 
   onMount(async () => {
     if (options) {
@@ -27,7 +20,7 @@
             if (!this.classList.contains('selected')) {
                 this.parentNode.querySelector('.custom-option.selected').classList.remove('selected');
                 this.classList.add('selected');
-                this.closest('.custom-select').querySelector('.custom-select__trigger span').textContent = this.textContent;
+                
             };
         });
       };
@@ -42,19 +35,38 @@
       }
     }
   });
+
+  function get_status(selected_zone) {
+    if (selected_zone) {
+      if (selected_zone == "Alle" || selected_zone == "Transport") {
+        return "";
+      } else {
+        let zoneID = (zones.find(zone => zone.name == selected_zone)).id;
+        let lifts_in_zone = lifts.filter(lift => lift.zone == zoneID);
+        for (var i=0; i<lifts_in_zone.length; i++) {
+          if (lifts_in_zone[i].status == "open") {
+            return "Åpen";
+          };
+        };
+        return "Stengt";
+      };  
+    } else {
+      return "Ukjent status";
+    };
+  }
 </script>
 
 <div bind:this={root}>
   {#if options} 
     <div class="custom-select-wrapper">
       <div class="custom-select">
-          <div class="custom-select__trigger"><span>{selected_option ? selected_option : init_option}</span>
+          <div class="custom-select__trigger"><div class="mobile-h3 selected-text">{selected_option !== "Vangslia" ? selected_option : init_option}</div>
               <div class="arrow"></div>
           </div>
           <div class="custom-options">
-              <div class="custom-option selected" data-value={init_option} on:click={() => selected_option = init_option}>{init_option}</div>
+              <div class="custom-option selected paragraph" data-value={init_option} on:click={() => selected_option = init_option}><div>{init_option}</div><div style={get_status(init_option) == "Åpen" ? "color: #2FC93E" : "color: #A0B2DC"}>{get_status(init_option)}</div></div>
               {#each options as option}
-                <div class="custom-option" data-value={option} on:click={() => selected_option = option}>{option}</div>
+                <div class="custom-option paragraph" data-value={option} on:click={() => selected_option = option}><div>{option}</div><div style={get_status(option) == "Åpen" ? "color: #2FC93E" : "color: #A0B2DC"}>{get_status(option)}</div></div>
               {/each}
           </div>
       </div>
@@ -68,100 +80,114 @@
      user-select: none;
      width: 100%;
 }
- .custom-select {
-     position: relative;
-     display: flex;
-     flex-direction: column;
-     border-width: 0 2px 0 2px;
-     border-style: solid;
-     border-color: #305c91;
+.selected-text {
+  color: white;
 }
- .custom-select__trigger {
-     position: relative;
-     display: flex;
-     align-items: center;
-     justify-content: space-between;
-     padding: 0 22px;
-     font-size: 20px;
-     font-weight: 300;
-     color: #3b3b3b;
-     height: 60px;
-     line-height: 60px;
-     background: #ffffff;
-     cursor: pointer;
-     border-width: 2px 0 2px 0;
-     border-style: solid;
-     border-color: #305c91;
+.custom-select {
+  position: relative;
+  display: flex;
+  flex-direction: column;
 }
- .custom-options {
-     position: absolute;
-     display: block;
-     top: 100%;
-     left: 0;
-     right: 0;
-     border: 2px solid #305c91;
-     border-top: 0;
-     background: #fff;
-     transition: all 0.5s;
-     opacity: 0;
-     visibility: hidden;
-     pointer-events: none;
-     z-index: 2;
+.custom-select__trigger {
+  position: relative;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 0.5rem;
+  font-size: 20px;
+  font-weight: 300;
+  color: #FFFFFF;
+  height: 34px;
+  line-height: 60px;
+  background: #2C3B6C;
+  cursor: pointer;
+  border-radius:4px;
+}
+.custom-options {
+  position: absolute;
+  display: block;
+  top: 100%;
+  left: 0;
+  right: 0;
+  border: 2px solid #2C3B6C;
+  border-top: 4px solid #2C3B6C;
+  border-radius: 4px;
+  margin-top: -4px;
+  background: #FFFFFF;
+  transition: all 0.5s;
+  opacity: 0;
+  visibility: hidden;
+  pointer-events: none;
+  z-index: 2;
 }
 :global(.open .custom-options) {  /*Global is needed to ensure that the Svelte compiler does not remove the CSS rule */
-     opacity: 1;
-     visibility: visible;
-     pointer-events: all;
+  opacity: 1;
+  visibility: visible;
+  pointer-events: all;
 }
- .custom-option {
-     position: relative;
-     display: block;
-     padding: 0 22px 0 22px;
-     font-size: 22px;
-     font-weight: 300;
-     color: #3b3b3b;
-     line-height: 60px;
-     cursor: pointer;
-     transition: all 0.5s;
+.custom-option {
+  position: relative;
+  display: block;
+  padding: 0.5rem 0.5rem 0.3rem 0.5rem;
+  cursor: pointer;
+  transition: all 0.5s;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;  
 }
- .custom-option:hover {
-     cursor: pointer;
-     background-color: #BABABA;
+.custom-option:not(:last-child):after {
+  content: "";
+  background: #A0B2DC;
+  position: absolute;
+  bottom: 0;
+  left: 0.5rem;
+  right: 0.5rem;
+  height: 1px;
 }
- .custom-option.selected {
-     color: #ffffff;
-     background-color: #305c91;
+.custom-option:hover {
+  cursor: pointer;
+  background-color: #F4F8FF;
 }
+.custom-option.selected {
+  font-weight: 700;
+}
+.paragraph {
+	font-size: 0.8125rem; /*13px*/
+	font-weight: normal;
+	line-height: 15px;
+}
+
+
 .arrow {
-     position: relative;
-     height: 15px;
-     width: 15px;
+  position: relative;
+  height: 10px;
+  width: 10px;
 }
- .arrow::before, .arrow::after {
-     content: "";
-     position: absolute;
-     bottom: 0px;
-     width: 0.15rem;
-     height: 100%;
-     transition: all 0.5s;
+.arrow::before, .arrow::after {
+  content: "";
+  position: absolute;
+  bottom: 0px;
+  width: 0.15rem;
+  height: 100%;
+  transition: all 0.5s;
 }
- .arrow::before {
-     left: -5px;
-     transform: rotate(-45deg);
-     background-color: #305c91;
+.arrow::before {
+  left: -3px;
+  transform: rotate(-45deg);
+  background-color: #FFFFFF;
 }
- .arrow::after {
-     left: 5px;
-     transform: rotate(45deg);
-     background-color: #305c91;
+.arrow::after {
+  left: 3px;
+  transform: rotate(45deg);
+  background-color: #FFFFFF;
 }
 :global(.open .arrow::before) {
-     left: -5px;
-     transform: rotate(45deg);
+  left: -3px;
+  transform: rotate(45deg);
 }
 :global(.open .arrow::after) {
-     left: 5px;
-     transform: rotate(-45deg);
+  left: 3px;
+  transform: rotate(-45deg);
 }
 
 
--- src/components/Zones.svelte
+++ src/components/Zones.svelte
@@ -3,22 +3,23 @@
     import { makeLiftsStore } from '../stores/LiftsStore';
     import { makeZoneStore } from '../stores/ZoneStore';
     import { onDestroy, onMount } from 'svelte';
+    import DropDown from './DropDown.svelte';
+    import { difficulty, difficultyIntToColor, difficultyToInt } from '../helpers/difficulty';
 
-    let unsubscribe = null;
+    let unsubscribe;
+    let selected_zone = "Vangslia";
 
     let trackStore = makeTracksStore();
     let tracks = [];
-
     let liftStore = makeLiftsStore();
     let lifts = [];
-
     let zoneStore = makeZoneStore();
     let zones = [];
 
-    let selectedZone = {
-        id: 1,
-        name: "Vangslia"
-    };
+    $: dropdownOptions = setDropdownOptions(zones);
+    $: activeZoneID = updateActiveZone(selected_zone, zones);
+    $: tracksInZone = updateTracksZone(activeZoneID, tracks); //Change to store so it can be accessed by map in index/large
+    $: liftsInZone = updateLiftsZone(activeZoneID, lifts); //Change to store so it can be accessed by map in index/large
 
     onMount(async () => {
         unsubscribe = trackStore.subscribe(data => {
@@ -38,55 +39,178 @@
             unsubscribe = null;
         };
     });
-</script>
 
-<div class="zones">
-    <div class="smallbold">Tilgjengelige løyper</div>
-    <div class="zone-select">
-        <div class="smallinfo">Skiområde:</div>
-        <div class="dropdown">
-            <select class="zone-dropdown" name="zones" id="zones" bind:value={selectedZone}>
-                {#each zones as zone}
-                <option value="{zone}">{zone.name}</option>
-                {/each}
-            </select>
-        </div>
+    function setDropdownOptions(zones) {
+        if (zones.length) {
+            dropdownOptions = zones.map(zone => zone.name);
+            dropdownOptions.shift()
+            dropdownOptions.push("Alle")
+            return dropdownOptions;
+        } else {
+            return [];
+        };
+    }
+
+    function updateActiveZone(selected_zone, zones) {
+        if (zones.length) {
+            if (selected_zone == "Alle") {
+                return false;
+            } else {
+                return (zones.find(zone => zone.name === selected_zone)).id
+            };
+        } else {
+            return false;
+        };
+    };
+
+    function updateTracksZone(activeZone, tracks) {
+        if (!activeZone) {
+            tracksInZone = tracks;  //Show all tracks for zone = "Alle"
+        } else {
+            tracksInZone = tracks.filter(track => track.zone === activeZone);
+        };
+        return tracksInZone;
+    };
+
+    function updateLiftsZone(activeZone, lifts) {
+        if (!activeZone) {
+            liftsInZone = lifts; //Show all lifts for zone = "Alle"
+        } else {
+            liftsInZone = lifts.filter(lift => lift.zone === activeZone);
+        };
+        return liftsInZone;
+    };
+</script> 
+
+<div class="card zones-card"> <!-- ADD GRID TO zones-card and use 2/3 rows-->
+
+    <div>
+        <div class="smallbold">Tilgjengelige løyper</div>
+        <div class="small-info zones-dropdown-header">Skiområde:</div>
+        {#if dropdownOptions.length > 0}
+            <DropDown bind:selected_option={selected_zone} options={dropdownOptions} zones={zones} lifts={lifts} />
+        {/if}
     </div>
-    <div class="tracks">
-        <p>Løyper i {selectedZone.name}</p>
-        <div class="list-item"><p>Jonasløypa (32) <span>lett</span><span class="smallbold">Åpen</span></p></div>
+
+    <div class="zones-card-bottom">
+        {#if tracksInZone.length > 0}
+            <div class="lift-track-card">
+                <p class="paragraph-bold">Løyper i {selected_zone}</p>
+                <div class="lift-track-container">
+                    <div class="scroll-container">
+                        {#each tracksInZone as track}
+                            <div class="lift-track-single">
+                                <div>
+                                    <p class="paragraph">{`${track.name} (${track.id}) `}</p>
+                                    <span class="paragraph-bold" style={`color: ${difficultyIntToColor[difficultyToInt[track.difficulty]]}`}>{(difficulty[track.difficulty]).toLowerCase()}</span>
+                                </div>
+                                <div>
+                                    {#if track.status === "closed"}
+                                        <span class="small-info">Stengt</span>
+                                        <i class="fas fa-times-circle closed-icon fa-lg"></i>
+                                    {:else}
+                                        <span class="small-info">Åpen</span>
+                                        <i class="fas fa-check-circle open-icon fa-lg"></i>
+                                    {/if}
+                                </div>
+                            </div>
+                        {/each}
+                    </div>
+                </div> 
+            </div>
+        {/if}
+
+        {#if liftsInZone.length > 0}
+            <div class="lift-track-card">
+                <p class="paragraph-bold">Heiser i {selected_zone}</p>
+                <div class="lift-track-container">
+                    <div class="scroll-container">
+                        {#each liftsInZone as lift}
+                            <div class="lift-track-single">
+                                <div>
+                                    <p class="paragraph">{`${lift.name} (${(lift.map_name).toUpperCase()})`}</p>
+                                </div>
+                                <div>
+                                    {#if lift.status === "closed"}
+                                        <span class="small-info">Stengt</span>
+                                        <i class="fas fa-times-circle closed-icon fa-lg"></i>
+                                    {:else}
+                                        <span class="small-info">Åpen</span>
+                                        <i class="fas fa-check-circle open-icon fa-lg"></i>
+                                    {/if}
+                                </div>
+                            </div>
+                        {/each}
+                    </div>
+                </div>
+            </div>
+        {/if}
     </div>
-    <div class="lifts">
-        <p>Heiser i {selectedZone.name}</p>
-        <div class="list-item"><p>Barneheis (P)<span class="smallbold">Åpen</span></p></div>
-    </div>
+        
 </div>
 
 <style>
-    .zones {
-        min-height: 506px;
-        max-width: 331px;
-        background: #F4F8FF;
+    .zones-card {
         border-radius: 9px;
-        padding: 17px;
+        margin: 1rem;
+        display: flex;
+        flex-direction: column;
     }
-    .zone-dropdown {
-        width: 100%;
-        border: none;
-        cursor: pointer;
-        background: #2C3B6C;
-        border: 2px solid #2C3B6C;
-        box-sizing: border-box;
-        border-radius: 4px;
-        color: #FFFFFF;
-        font-family: Work Sans;
-        font-style: normal;
-        font-weight: 600;
-        font-size: 18px;
-        line-height: 21px;
+    .zones-dropdown-header {
+        margin: 1rem 0 0.5rem 0;
+    }
+
+    .zones-card-bottom {
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column;
+        margin-top: 1rem;
+    }
+    .lift-track-card {
+        margin: 1rem 0;
+    }
+    .lift-track-container {
+        margin-top: 0.4rem;
+        border: 1px solid #DBE3F5;
+        border-radius: 6px;
         padding: 0.5rem;
     }
-    .list-item {
-        border-bottom: 0.840336px solid #A0B2DC;
+    .lift-track-single {
+        border-bottom: 1px #A0B2DC solid;
+        padding: 0.5rem 0 0.3rem 0;
+        display: flex;
+        flex-direction: row;
+        justify-content: space-between;
+    }
+    .lift-track-single:last-child {
+        border-bottom: none;
+    }
+    .paragraph {
+        display: inline;
+    }
+    .closed-icon {
+        color: #C7C7C7;
+        font-size: 18px;
+    }
+    .open-icon {
+        color: #2FC93E;
+        font-size: 18px;
+    }
+    .scroll-container {
+        padding-right: 0.5rem; /* padding between scrollbar and track/lift single */
+        max-height: 200px;
+        overflow-y: auto;
+    }
+    ::-webkit-scrollbar {
+        -webkit-appearance: none;
+        width: 4px;
+    }
+    ::-webkit-scrollbar-thumb {
+        background-color: #2C3B6C;
+        border-radius: 2px;
+    }
+    ::-webkit-scrollbar-track {
+        border: 5px #A0B2DC solid;
+        border-radius: 2px;
     }
 </style>
