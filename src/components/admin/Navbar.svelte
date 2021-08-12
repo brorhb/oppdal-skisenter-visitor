cf5ba094ea6e96fd4bcd4d4bb0cb8e7d1030c181
mathildehaugum <mathilde_haugum@hotmail.com>
2021-08-12 10:24:18 +0200

    Update admin home page and navbar

--- src/components/admin/Navbar.svelte
+++ src/components/admin/Navbar.svelte
@@ -1,49 +1,89 @@
 <script>
     import { navigateTo } from 'svelte-router-spa'
     export let currentRoute
-    const logout = () => {
-        localStorage.removeItem('user');
-        navigateTo(`/login`)
-    }
+    //const logout = () => {
+    //    localStorage.removeItem('user');
+    //    navigateTo(`/login`)
+    //}
+    let root;
+
+    function open_hamburger() {
+        var navbar = document.getElementById("hamburger");
+        navbar.classList.toggle('responsive');
+        console.log(navbar.classList);
+    };
+
 </script>
-<div class="admin-nav">
+<div class="admin-nav" bind:this={root}>
+    <div class="max-width-div">
         <div class="nav-left">
-            <h4 class="{currentRoute.path == "/admin" ? "active" : ""}" on:click="{() => navigateTo("/admin")}">Hjem</h4>
+            <h4 class="{currentRoute.path == "/admin" ? "active" : ""}" on:click="{() => navigateTo("/admin")}"><i class="fas fa-home home-icon"></i>Hjem</h4>
         </div>
-        <div class="nav-right">
-            <h4 class="{currentRoute.path == "/admin/skredvarsel" ? "active" : ""}" on:click="{() => navigateTo("/admin/skredvarsel")}">Skredvarsel</h4>
-            <h4 class="{currentRoute.path == "/admin/snoforhold" ? "active" : ""}" on:click="{() => navigateTo("/admin/snoforhold")}">Snøforhold</h4>
-            <h4 class="{currentRoute.path == "/admin/meldinger" ? "active" : ""}" on:click="{() => navigateTo("/admin/meldinger")}">Meldinger</h4>
-            <h4 class="{currentRoute.path == "/admin/status" ? "active" : ""}" on:click="{() => navigateTo("/admin/status")}">Åpne/steng</h4>
+        <div class="nav-right" id="hamburger">
+            <div class="{currentRoute.path == "/admin/skredvarsel" ? "active" : ""} nav-text" on:click="{() => navigateTo("/admin/skredvarsel")}">Skredvarsel</div>
+            <div class="{currentRoute.path == "/admin/snoforhold" ? "active" : ""} nav-text" on:click="{() => navigateTo("/admin/snoforhold")}">Snøforhold</div>
+            <div class="{currentRoute.path == "/admin/meldinger" ? "active" : ""} nav-text" on:click="{() => navigateTo("/admin/meldinger")}">Meldinger</div>
+            <div class="{currentRoute.path == "/admin/status" ? "active" : ""} nav-text" on:click="{() => navigateTo("/admin/status")}">Åpne/steng</div>
+            <div class="icon" on:click={open_hamburger}>
+                <i class="fas fa-bars fa-2x"></i>
+            </div>
         </div>
+    </div>
 </div>
 
 <style>
      .admin-nav {
         width: 100%;
         height: 69px;
-        background: #2C3B6C;
+        background: #2C3B6C;    
+    }
+    .max-width-div {
+        max-width: 1200px;
+        margin: 0 auto 0 auto;
+        width: 100%;
         display: flex;
-        align-content: space-around;
         justify-content: space-between;
-        align-items: flex-end;        
+        align-items: flex-end;    
     }
     .nav-right {
         display: flex;
     }
-    .nav-right > h4, .nav-left > h4 {
+    .nav-left > h4 {
+        margin: 24px 0;
+        cursor: pointer;
+    }
+    .nav-text {
         margin: 24px;
         cursor: pointer;
+    }
+    .nav-text:last-child {
+        margin-right: 0;
+    }
+    .nav-right > .icon {
+        display: none;
     }
     h4 {
         color: #ffffff;
     }
+    .home-icon {
+        padding-right: 0.5rem;
+    }
+
+    :global(.responsive .nav-text) {
+        display: block
+    }
+
     @media only screen and (max-width: 800px) {
+        .nav-right > .icon {
+            display: flex;
+            padding-right: 1rem;
+            margin: 24px 0;
+            color: #FFFFFF;
+        }
+        .nav-text {
+            display: none;
+        }
         
     }
-    @media only screen and (max-width: 1000px) {
-        .admin-nav {
-            justify-content: left;
-        }
-    }
+   
 </style>
--- src/views/admin/AdminHome.svelte
+++ src/views/admin/AdminHome.svelte
@@ -1,33 +1,44 @@
+<script>
+    import { navigateTo } from 'svelte-router-spa';
+</script>
+
 
 <div class="admin-home">
     <h1>Admin</h1>
     <div class="button-container">
-        <div class="button">
-            <i class="fas fa-toggle-on"></i>
-            <h2>Åpne/steng løyper og heiser</h2>
+        <div class="home-button" on:click="{() => navigateTo("/admin/status")}">
+            <div class="home-button-left">
+                <img src="../../../assets/toggle.svg" class="home-icons" alt="toggle-ikon">
+                <div class="big-paragraph">Åpne/steng løyper og heiser</div>
+            </div>
             <i class="fas fa-angle-right"></i>
         </div>
-        <div class="button">
-            <i class="fas fa-toggle-on"></i>
-            <h2>Oppdater snøforhold</h2>
+        <div class="home-button" on:click="{() => navigateTo("/admin/snoforhold")}">
+            <div class="home-button-left">
+                <img src="../../../assets/snowflake.svg" class="home-icons" alt="snowflake-ikon">
+                <div class="big-paragraph">Oppdater snøforhold</div>
+            </div>
             <i class="fas fa-angle-right"></i>
         </div>
-        <div class="button">
-            <i class="fas fa-toggle-on"></i>
-            <h2>Oppdater meldinger</h2>
+        <div class="home-button" on:click="{() => navigateTo("/admin/meldinger")}">
+            <div class="home-button-left">
+                <img src="../../../assets/message.svg" class="home-icons" alt="message-ikon">
+                <div class="big-paragraph">Oppdater meldinger</div>
+            </div>
             <i class="fas fa-angle-right"></i>
         </div>
-        <div class="button">
-            <i class="fas fa-toggle-on"></i>
-            <h2>Skredvarsel på tavler</h2>
+        <div class="home-button" on:click="{() => navigateTo("/admin/skredvarsel")}">
+            <div class="home-button-left">
+                <img src="../../../assets/table.svg" class="home-icons" alt="table-ikon">
+                <div class="big-paragraph">Skredvarsel på tavler</div>
+            </div>
             <i class="fas fa-angle-right"></i>
         </div>
     </div>
+
     <div class="medium-button">
-        <div class="button">
-            <i class="fas fa-toggle-on"></i>
-            <h2>Sett tavler på mørk</h2>
-        </div>
+        <img src="../../../assets/onoff.svg" alt="toggle-ikon">
+        <div class="small-info">Sett tavler på mørk</div>
     </div>
     <div class="small-button-container">
         <div class="small-button">
@@ -44,6 +55,7 @@
     .admin-home {
         display: flex;
         flex-direction: column;
+        margin-top: 8rem;
     }
     .admin-home > h1 {
         font-family: Work Sans;
@@ -52,42 +64,55 @@
         font-size: 35.8879px;
         line-height: 42px;
         color: #2C3B6C;
-        margin: 8px;
+        margin: 8px 0;
     }
     .button-container {
         display: flex;
         flex-wrap: wrap;
-        justify-content: space-between;
+        justify-content: flex-start;
         align-items: center;
         width: 100%;
+        margin: 1rem 0 8rem 0;
     }
-    .button {
+    .home-button {
         min-width: 38%;
+        min-height: 70px;
         display: flex;
-        justify-content: space-evenly;
+        justify-content: space-between;
         background: #FFFFFF;
         padding: 22px;
-        margin: 8px;
+        margin: 8px 20px 8px 0;
         cursor: pointer;
-        
+        align-items: center;
     }
-    .button > * {
+    .home-button-left {
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+    }
+    .home-button-left > img  {
+        padding-right: 1rem;
+    }
+    .home-button > * {
         margin: 8px;
     }
-    .button > i {
-        font-size: 28px;
-        color: #2C3B6C;
-    }
-    .button > h2 {
-        font-family: Work Sans;
-        font-style: normal;
-        font-weight: normal;
-        font-size: 19.1402px;
-        line-height: 22px;
-        color: #2C3B6C;
+    .home-icons {
+        width: 50px;
+        height: auto;
     }
     .medium-button {
-        max-width: 215px;
+        max-height: 50px;
+        max-width: 200px;
+        display: flex;
+        justify-content: flex-start;
+        background: #FFFFFF;
+        padding: 22px;
+        margin-bottom: 10rem;
+        cursor: pointer;
+        align-items: center;
+    }
+    .medium-button > img {
+        padding-right: 1rem;
     }
     .small-button-container {
         display: flex;
@@ -104,15 +129,4 @@
         color: #2C3B6C;
         cursor: pointer;
     }
-    @media only screen and (max-width: 600px) {
-        .button {
-            width: 100%;
-        }
-        .medium-button > .button {
-            max-width: 180px;
-            height: 40px;
-        }
-    }
-
-
 </style>
