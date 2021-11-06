3f403e19ea7265abc876547c4c0d1d11f06489a0
Bror Brurberg <bror@hey.com>
2021-11-06 11:26:44 +0100

    added page for full screen viewing of one camera

--- src/routes.js
+++ src/routes.js
@@ -1,85 +1,95 @@
-import Layout from './components/Layout.svelte'
-import AdminLayout from './components/admin/AdminLayout.svelte'
-import Login from './views/admin/Login.svelte'
-import AdminIndex from './views/admin/index.svelte'
-import AdminTracks from "./views/admin/Tracks.svelte"
-import AdminLifts from "./views/admin/Lifts.svelte"
-import EditItem from './views/admin/EditItem.svelte'
-import AdminAlerts from './views/admin/Alerts.svelte'
-import AdminWebcamera from './views/admin/Webcamera.svelte'
-import EditWebcamera from './views/admin/EditCamera.svelte'
-import AdminSnowConditions from './views/admin/SnowConditions.svelte'
-import index from './views/index.svelte'
-import AdminHome from './views/admin/AdminHome.svelte'
-import AdminStatus from './views/admin/Status.svelte'
-import AdminAvalanche from './views/admin/Avalanche.svelte'
-import AdminSettings from './views/admin/Settings.svelte'
+import Layout from './components/Layout.svelte';
+import AdminLayout from './components/admin/AdminLayout.svelte';
+import WebcamFullscreen from './views/WebcamFullscreen.svelte';
+import Login from './views/admin/Login.svelte';
+import AdminIndex from './views/admin/index.svelte';
+import AdminTracks from './views/admin/Tracks.svelte';
+import AdminLifts from './views/admin/Lifts.svelte';
+import EditItem from './views/admin/EditItem.svelte';
+import AdminAlerts from './views/admin/Alerts.svelte';
+import AdminWebcamera from './views/admin/Webcamera.svelte';
+import EditWebcamera from './views/admin/EditCamera.svelte';
+import AdminSnowConditions from './views/admin/SnowConditions.svelte';
+import index from './views/index.svelte';
+import AdminHome from './views/admin/AdminHome.svelte';
+import AdminStatus from './views/admin/Status.svelte';
+import AdminAvalanche from './views/admin/Avalanche.svelte';
+import AdminSettings from './views/admin/Settings.svelte';
 function userIsAdmin() {
-  const user = localStorage.getItem("user")
-  return user
+  const user = localStorage.getItem('user');
+  return user;
 }
 
 export const routes = [
   {
-    name: "/",
+    name: '/',
     component: index,
-    layout: Layout
+    layout: Layout,
   },
   {
-    name: "admin",
+    name: '/kamera',
+    nestedRoutes: [
+      {
+        name: '/:id',
+        component: WebcamFullscreen,
+      },
+    ],
+  },
+  {
+    name: 'admin',
     component: AdminIndex,
     onlyIf: { guard: userIsAdmin, redirect: '/login' },
     nestedRoutes: [
-      { name: 'index', component: AdminHome},
-      { name: '/meldinger', component: AdminAlerts},
-      { name: 'snoforhold', component: AdminSnowConditions},
+      { name: 'index', component: AdminHome },
+      { name: '/meldinger', component: AdminAlerts },
+      { name: 'snoforhold', component: AdminSnowConditions },
       { name: '/status', component: AdminStatus },
       { name: '/skredvarsel', component: AdminAvalanche },
       { name: '/instillinger', component: AdminSettings },
       { name: '/instillinger/webkamera', component: AdminWebcamera },
-      { 
-        name: '/webkamera', 
+      {
+        name: '/webkamera',
         nestedRoutes: [
           {
-            name: "index",
-            component: AdminWebcamera
+            name: 'index',
+            component: AdminWebcamera,
           },
           {
-            name: "/:id",
-            component: EditWebcamera
-          }
-        ]
+            name: '/:id',
+            component: EditWebcamera,
+          },
+        ],
       },
       {
-        name: "/loyper",
+        name: '/loyper',
         nestedRoutes: [
           {
-            name: "index",
-            component: AdminTracks
+            name: 'index',
+            component: AdminTracks,
           },
           {
-            name: "/:id",
-            component: EditItem
-          }
-        ]
+            name: '/:id',
+            component: EditItem,
+          },
+        ],
       },
       {
-        name: "/heiser",
+        name: '/heiser',
         nestedRoutes: [
           {
-            name: "index",
-            component: AdminLifts
+            name: 'index',
+            component: AdminLifts,
           },
           {
-            name: "/:id",
-            component: EditItem
-          }
-        ]
-      }
-    ]
+            name: '/:id',
+            component: EditItem,
+          },
+        ],
+      },
+    ],
   },
   {
-    name: "/login",
-    component: Login
+    name: '/login',
+    component: Login,
   },
-]
+];
