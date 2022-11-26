acfbf0f14ae5b74d7c71d9512362cdfbba30f493
Bror Brurberg <bror@hey.com>
2022-11-26 10:06:35 +0100

    fix: agjusted padding

--- components/user/WeatherAndCameras.vue
+++ components/user/WeatherAndCameras.vue
@@ -1,7 +1,7 @@
 <template>
   <MaxWrapper>
     <div>
-      <h2 class="text-2xl font-bold sm:px-4">Vær og føre</h2>
+      <h2 class="text-2xl font-bold px-2">Vær og føre</h2>
       <div class="flex md:flex-row w-full flex-col">
         <div class="w-full md:h-auto p-4 md:px-0 h-96 md:w-2/3">
           <Cameras class="flex py-4" :cameras="cameras"></Cameras>
