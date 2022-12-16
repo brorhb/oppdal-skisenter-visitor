7d828f7f83a7bd18e3e5dc23983baf8b7609809b
Bror Brurberg <bror@hey.com>
2022-12-16 16:55:19 +0100

    adjusted banner color

--- components/user/Conditions.vue
+++ components/user/Conditions.vue
@@ -1,6 +1,6 @@
 <template>
   <MaxWrapper class="px-3 py-6" v-if="conditions.conditions.length">
-    <div class="flex flex-col justify-center bg-blue-400 dark:bg-blue-700 p-4 w-full rounded-2xl">
+    <div class="flex flex-col justify-center bg-blue-100 dark:bg-blue-700 p-4 w-full rounded-2xl">
       <h3 class="font-semibold text-2xl text-black-900">Snøforhold</h3>
       <div v-for="(condition, index) in conditions.conditions" :key="index" class="flex justify-start py-4 text-sm">
         <p class="text-blue-700 dark:text-blue-300 pr-3">{{get_publish_date(condition.timestamp)}}</p>
